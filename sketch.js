a8cbd403d550f4f98e13de9d11fb353bb5ca2edb
sonalisadhukhan88 <74597546+sonalisadhukhan88@users.noreply.github.com>
2021-05-18 11:15:39 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,16 +12,6 @@ function draw() {
   background(255,255,255);  
   mr.x = World.mouseX;
   mr.y=World.mouseY;
-  if(mr.x-fr.x < mr.width/2+fr.width/2
-    && fr.x-mr.x < mr.width/2+fr.width/2
-    && mr.y-fr.y < mr.height/2+fr.height/2
-    && fr.y-mr.y < mr.height/2+fr.height/2){
-    mr.shapeColor = "red";
-fr.shapeColor = "red";
-  }
-  else{
-    mr.shapeColor = "green";
-fr.shapeColor = "green";
-  }
+  
   drawSprites();
 }
